874be5a29bc9ac06401647264270765facbb27f0
srungavarapu <101197151+srungavarapu@users.noreply.github.com>
2022-04-04 22:07:13 +0800

    Update ec2-launch.sh

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -13,21 +13,3 @@
 # --block-device-mapping ...:false to leave the disk image around after terminating instance
 EC2_RUN_RESULT=$(aws ec2 run-instances --image-id ami-0bb6af715826253bf --security-group-ids sg-008147de9733254d3 --instance-type t2.micro)
 
-INSTANCE_NAME=$(echo ${EC2_RUN_RESULT} | sed 's/RESERVATION.*INSTANCE //' | sed 's/ .*//')
-
-times=0
-echo
-while [ 5 -gt $times ] && ! $INSTANCE_NAME | grep -q "running"
-do
-  times=$(( $times + 1 ))
-  echo Attempt $times at verifying $INSTANCE_NAME is running...
-done
-
-echo
-
-if [ 5 -eq $times ]; then
-  echo Instance $INSTANCE_NAME is not running. Exiting...
-  exit
-fi
-
-ec2-associate-address $IP_ADDRESS -i $INSTANCE_NAME
